a37560848f670da17cd2e2ffcad1422becc569d0
jihoo <janett1005@naver.com>
2023-05-17 03:33:06 +0900

    Bhaptics second setting
    1. OVRController error발생
    2. Bhaptics 기기로 test필요(incendio)-incendio성공 시 bhaptics작동하도록 설정함
    3. 마법 repello, immobulus 추가

--- BeHarryPotter/Assets/Bhaptics/SDK2/Scripts/AutoGenerated/BhapticsEvent.cs
+++ BeHarryPotter/Assets/Bhaptics/SDK2/Scripts/AutoGenerated/BhapticsEvent.cs
@@ -17,18 +17,26 @@ namespace Bhaptics.SDK2
     public class BhapticsEvent
     {
         
-        public const string ATTACKEDRIFLE_BODY = "attackedrifle_body";
-        
-        public const string ATTACKEDSNIPER_BODY = "attackedsniper_body";
-        
-        public const string IN_THE_SMOKESHELL = "in_the_smokeshell";
-        
-        public const string HEARTBEAT = "heartbeat";
+        public const string ATTACKEDSHOTGUN_BODY = "attackedshotgun_body";
         
         public const string ATTACKEDPISTOL_BODY = "attackedpistol_body";
         
-        public const string HEARTBEAT_FAST = "heartbeat_fast";
+        public const string HEARTBEAT = "heartbeat";
         
-        public const string ATTACKEDSHOTGUN_BODY = "attackedshotgun_body";
+        public const string ATTACKEDRIFLE_BODY = "attackedrifle_body";
+        
+        public const string IN_THE_SMOKESHELL = "in_the_smokeshell";
+        
+        public const string ATTACKED_BACK = "attacked_back";
+        
+        public const string ATTACKED_LEFT = "attacked_left";
+        
+        public const string ATTACKED_FRONT = "attacked_front";
+        
+        public const string ATTACKEDSNIPER_BODY = "attackedsniper_body";
+        
+        public const string ATTACKED_RIGHT = "attacked_right";
+        
+        public const string HEARTBEAT_FAST = "heartbeat_fast";
     }
 }
--- BeHarryPotter/Assets/Scripts/MagicHandler.cs
+++ BeHarryPotter/Assets/Scripts/MagicHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AttackDefense;
 
 public class MagicHandler : MonoBehaviour
 {
@@ -12,12 +13,27 @@ public class MagicHandler : MonoBehaviour
         if (values[0] == "flame") { Flame(); };
         //마법 : Wingardium Leviosa
         if (values[0] == "levitate") { Levitate(); };
+        //마법 : Repello
+        if (values[0] == "defense") { Defense(); };
+        //마법 : Immobulus
+        if (values[0] == "freeze") { Freeze(); };
     }
 
     void Flame(){
+        Debug.Log("This is flame");
 
+        var at = new AttackedHandler();
+        at.Attacked_Bhaptics(AttackedHandler.attackedDirection.Left);
         }
     void Levitate() {
-
+        Debug.Log("This is levitate");
+    }
+    void Defense()
+    {
+        Debug.Log("This is defense");
+    }
+    void Freeze()
+    {
+        Debug.Log("This is freeze");
     }
 }
