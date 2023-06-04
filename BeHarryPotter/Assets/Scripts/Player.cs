39845a3ef4189b3a8e0016e119caede6ebb5457f
jihoo <janett1005@naver.com>
2023-06-04 14:25:09 +0900

    player/defense script

--- BeHarryPotter/Assets/Scripts/Player.cs
+++ BeHarryPotter/Assets/Scripts/Player.cs
@@ -1,49 +1,50 @@
-//player collider 충돌 감지 후
-//공격Type+충돌 방향+방어 성공 여부 따라 
-//bhaptics(attacked) or sleeve(defense) signal
+//player collider 충돌 감지 후(방어 실패)
+//AttackDirection 따라 
+//bhaptics(attacked) signal
 using Oculus.Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using HapticsHandler;
 
 public class Player : MonoBehaviour
 {
-    //void Start()
-    //{
-       
-    //}
-    //void Update()
-    //{
-        
-    //}
+    private void OnCollisionEnter(Collision other)    //collisor 범위 안에 닿을 시, 공격체도 Collider component 필수
+    { 
+        var AttackDirection = ChooseAttackDirection(other);    //충돌 방향
+        var at = new BHapticsHandler();
 
-    private void OnTriggerEnter(Collider other)    //collisor 범위 안에 닿을 시, 공격체도 Collider component 필수
-    {
-
-        var AttackType = ChooseAttackType(other);
-        
-
-        //Vector2 direction = other.
-        //    GetContact(0).normal;
-        //If(direction.x == 1) print(“right”);
-        //If(direction.x == -1) print(“left”);
-        //If(direction.y == 1) print(“up”);
-        //If(direction.y == -1) print(“down”);
+        if (AttackDirection == "right")
+        {
+            Debug.Log("오른쪽이 아야합니다!");
+            at.Attacked_Bhaptics(BHapticsHandler.attackedDirection.Right);
+        }
+        else if(AttackDirection == "left")
+        {
+            Debug.Log("왼쪽이 아야합니다!");
+            at.Attacked_Bhaptics(BHapticsHandler.attackedDirection.Left);
+        }
+        else if (AttackDirection == "front")
+        {
+            Debug.Log("앞이 아야합니다!");
+            at.Attacked_Bhaptics(BHapticsHandler.attackedDirection.Front);
+        }
+        else if (AttackDirection == "back")
+        {
+            Debug.Log("뒤가 아야합니다!");
+            at.Attacked_Bhaptics(BHapticsHandler.attackedDirection.Back);
+        }
     }
-    private string ChooseAttackType(Collider other)
+
+    private string ChooseAttackDirection(Collision other)
     {
-        if (other.gameObject.CompareTag("FireBall"))    //불 공격이랑 충돌 시
-        {
-            return "FireBall";
-        }
-        else if (other.gameObject.CompareTag("IceBall"))    //얼음 공격이랑 충돌 시
-        {
-            return "IceBall";
-        }
-        else
-        {
-            return "None";
-        }
+        Vector3 direction = other.GetContact(0).normal;
+
+        if (direction.x == 1) { return  "right"; }
+        else if (direction.x == -1) { return "left"; }
+        else if (direction.z == 1) { return "front"; }
+        else if (direction.z == -1) { return "back"; }
+        else { return "None"; };
     }
 }
