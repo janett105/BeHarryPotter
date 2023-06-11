d2a799c7b4d60d9d48e9d05aedd21b45557746e1
jihoo <janett1005@naver.com>
2023-06-11 14:42:03 +0900

    armsleeve test

---
+++ BeHarryPotter/Assets/Ardity/Scripts/Samples/SampleUserPolling_ReadWrite.cs
@@ -0,0 +1,85 @@
+/**
+ * Ardity (Serial Communication for Arduino + Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+
+using UnityEngine;
+using System.Collections;
+
+namespace HapticsHandler
+{
+    public class SampleUserPolling_ReadWrite : MonoBehaviour
+    {
+        public SerialController serialController;
+
+        void Start()
+        {
+            serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+        }
+
+        public void firesignal()
+        {
+            Debug.Log("나지금 fire보낸다?");
+            serialController.SendSerialMessage("A");
+
+            string message = serialController.ReadSerialMessage();
+
+            if (message == null)
+                return;
+
+            // Check if the message is plain data or a connect/disconnect event.
+            if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+                Debug.Log("Connection established");
+            else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+                Debug.Log("Connection attempt failed or disconnection detected");
+            else
+                Debug.Log("Message arrived: " + message);
+        }
+        public void icesignal()
+        {
+            Debug.Log("나지금 ice보낸다?");
+            serialController.SendSerialMessage("Z");
+
+            string message = serialController.ReadSerialMessage();
+
+            if (message == null)
+                return;
+
+            // Check if the message is plain data or a connect/disconnect event.
+            if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+                Debug.Log("Connection established");
+            else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+                Debug.Log("Connection attempt failed or disconnection detected");
+            else
+                Debug.Log("Message arrived: " + message);
+        }
+
+
+        //방어막 object script(Defense)
+        private void OnCollisionEnter(Collision other)    //방어막 object : collider, 이 script있으면 됨 
+        {
+            var AttackType = ChooseAttackType(other);
+
+            if (AttackType == "FireBall")
+            {
+                Debug.Log("파이어볼 방어");
+                firesignal();
+
+            }
+            else if (AttackType == "IceBall")
+            {
+                Debug.Log("아이스볼 방어");
+                icesignal();
+            }
+        }
+        private string ChooseAttackType(Collision other)
+        {
+            if (other.collider.gameObject.CompareTag("FireBall")) { return "FireBall"; }
+            else if (other.collider.gameObject.CompareTag("IceBall")) { return "IceBall"; }
+            else { return "None"; }
+        }
+    }
+}
