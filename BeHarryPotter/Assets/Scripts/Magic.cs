b315eeac01162ec0ea9cc0da3c882e25c715c792
Eunhwa Yi <51324203+eunhwa19@users.noreply.github.com>
2023-06-12 00:31:18 +0900

    Update Player Attack

--- BeHarryPotter/Assets/Scripts/Magic.cs
+++ BeHarryPotter/Assets/Scripts/Magic.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Magic : MonoBehaviour
 {
     public GameObject target;
-
     private Rigidbody magicRigid;
 
     public float speed;
@@ -18,16 +17,35 @@ public class Magic : MonoBehaviour
         if(other.collider.gameObject.CompareTag("Plane"))
         {
             Debug.Log("Plane이야");
-
             Destroy(gameObject, 1f);
         }
-
         else if (other.collider.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Player야");
-            Destroy(gameObject);
+            if (gameObject.tag == "FireBall") //적공격
+            {
+                //Debug.Log("Player야");
+                Destroy(gameObject);
+            }
+            else if(gameObject.tag == "PlayerMagic") //플레이어 공격 
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+        }
+        else if(other.collider.gameObject.CompareTag("Enemy"))
+        {
+            if (gameObject.tag == "FireBall") //적공격
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+            else if (gameObject.tag == "PlayerMagic") //플레이어 공격 
+            {
+                Destroy(gameObject);
+            }
         }
     }
+
+    //magic이 "PlayerAttack"이면 player와 충돌해도 사라지면 안 됨, enemy와 충돌하면 사라져야함. 
+    //magic이 fireball이면 지금 충돌처리 그대로 
 
     // Start is called before the first frame update
     void Start()
@@ -50,7 +68,6 @@ public class Magic : MonoBehaviour
             GameObject tmp = GameObject.FindGameObjectWithTag("Player");
             target = tmp;
         }
-        //transform.Translate((transform.position - target.transform.position) * speed * Time.deltaTime);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
