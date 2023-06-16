a42c008c46d924a01b0a259b16612b2fe91144f0
SNG0407 <51324203+eunhwa19@users.noreply.github.com>
2023-06-16 11:28:41 +0900

    small change

--- BeHarryPotter/Assets/Scripts/MagicHandler.cs
+++ BeHarryPotter/Assets/Scripts/MagicHandler.cs
@@ -26,17 +26,20 @@ public class MagicHandler : MonoBehaviour
     
     void Flame()
     {
+        OVRInput.SetControllerVibration(1f, 2f, OVRInput.Controller.RHand);
         GameObject magic = Instantiate(firePrefab, magicPos.transform.position, magicPos.transform.rotation);
         Debug.Log("This is flame");
     }
     void Defense()
     {
+        OVRInput.SetControllerVibration(1f, 2f, OVRInput.Controller.RHand);
         GameObject magic = Instantiate(defensePrefab, new Vector3(0.74f, 7.13f, -0.67f), Quaternion.Euler(new Vector3(90,0,0)));
         Debug.Log("This is defense");
-        Destroy(magic, 10000000f);
+        Destroy(magic, 5f);
     }
     void Freeze()
     {
+        OVRInput.SetControllerVibration(1f, 2f, OVRInput.Controller.RHand);
         GameObject magic = Instantiate(icePrefab, magicPos.transform.position, magicPos.transform.rotation);
         Debug.Log("This is freeze");
     }
