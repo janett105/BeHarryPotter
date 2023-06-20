268db41c540b7a88b0f1709a3887a15c86fbcfd2
Eunhwa Yi <51324203+eunhwa19@users.noreply.github.com>
2023-06-20 21:51:14 +0900

    remove file

--- BeHarryPotter/Assets/Scripts/MagicHandler.cs
+++ BeHarryPotter/Assets/Scripts/MagicHandler.cs
@@ -41,14 +41,14 @@ public class MagicHandler : MonoBehaviour
     
     void Flame()
     {
-        OVRInput.SetControllerVibration(1f, 2f, OVRInput.Controller.RHand);
+        OVRInput.SetControllerVibration(1f, 10f, OVRInput.Controller.RHand);
         GameObject magic = Instantiate(icePrefab, magicPos.transform.position, magicPos.transform.rotation);
         Debug.Log("���� ����");
         playsoundeffect(1);
     }
     void Defense()
     {
-        OVRInput.SetControllerVibration(1f, 2f, OVRInput.Controller.RHand);
+        OVRInput.SetControllerVibration(1f, 10f, OVRInput.Controller.RHand);
         GameObject magic = Instantiate(defensePrefab, new Vector3(0.74f, 7.13f, -0.67f), Quaternion.Euler(new Vector3(0,0,0)));
         Debug.Log("This is defense");
         Destroy(magic, 5f);
